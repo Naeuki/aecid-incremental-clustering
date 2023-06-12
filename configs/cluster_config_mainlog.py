a9a21d5af296bbf322a7ee04c700e58794807330
Naeuki <watanabe0919tenn1s@gmail.com>
2023-06-12 16:06:09 +0900

    Changes to be committed:

--- configs/cluster_config_mainlog.py
+++ configs/cluster_config_mainlog.py
@@ -1,5 +1,5 @@
 st = 0.51 # Similarity threshold for clustering [0, 1]
-input_file = 'c:\Users\Adapt\aecid-incremental-clustering\aecid-incremental-clustering\data\in\mainlog' # Path to input log file
-output_file = 'data\out\clusters(test).txt' # Path to output file
+input_file = 'Users/Adapt/aecid-incremental-clustering/data/in/mainlog' # Path to input log file
+output_file = 'Users/Adapt/aecid-incremental-clustering/data/indata/out/clusters(test).txt' # Path to output file
 timestamp_length = 19 # Length of time stamp at beginning of log line that will be removed; set to -1 for no timestamp
 write_members = True # Set whether output file will only contain cluster representatives or all cluster members [True, False]
